b2900b4d9de1f1d95146c9515e715d5eff9ee154
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2024-10-27 02:32:59 -0300

    Borrado de archivos

--- tpspi/segundoparcial/tp11pi/TADs/socialADT/socialADT.c
+++ tpspi/segundoparcial/tp11pi/TADs/socialADT/socialADT.c
@@ -7,4 +7,11 @@ socialADT newSocial();
 
 void freeSocial(socialADT soc);
 
+void addPerson(socialADT soc, const char * name);
+
+void addRelated(socialADT soc, const char * name, const char * related);
+
+char ** related(const socialADT soc, const char * person);
+
+char ** persons(const socialADT soc);
 

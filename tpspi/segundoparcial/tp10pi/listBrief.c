a1cefdb47d391f881f6c1a29150a04d48a32f075
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2024-11-11 16:36:22 -0300

    Agrego ejercicios importantes para el parcial de la guia 10

---
+++ tpspi/segundoparcial/tp10pi/listBrief.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include "utillist.h"
+
+typedef struct nodeBrief * TListBrief; 
+
+typedef struct nodeBrief {
+		int elem;
+		size_t count;
+		struct nodeBrief * tail;
+} TNodeBrief;
+
+
+TListBrief comprimeList(const TList list) {
+    if (list == NULL) return NULL;
+    TListBrief aux = comprimeList(list->tail);
+    if (aux == NULL || aux->elem != list->elem) {
+        TListBrief newNode = malloc(sizeof(*newNode));
+        newNode->elem = list->elem;
+        newNode->count = 1;
+        newNode->tail = aux;
+        return newNode;
+    }
+    aux->count++;
+    return aux;
+}
+
+// Auxiliar para que pase el test con sanitizer
+void freeListBrief(TListBrief lb) {
+   if ( lb == NULL)
+     return;
+   freeListBrief(lb->tail);
+   free(lb);
+}
+
+int main(void) {
+
+  int v[] = {1,1,2,3,3,3,3,3,10,100,100,998};
+  TList list = fromArray(v, sizeof(v)/sizeof(v[0]));
+  TListBrief bf = comprimeList(list);
+
+  TListBrief aux = bf;
+  assert(aux->elem ==1 && aux->count == 2);
+
+  aux = aux->tail;
+  assert(aux->elem ==2 && aux->count == 1);
+
+  aux = aux->tail;
+  assert(aux->elem ==3 && aux->count == 5);
+
+  aux = aux->tail;
+  assert(aux->elem ==10 && aux->count == 1);
+
+  aux = aux->tail;
+  assert(aux->elem ==100 && aux->count == 2);
+
+  aux = aux->tail;
+  assert(aux->elem ==998 && aux->count == 1);
+
+  aux = aux->tail;
+  assert(aux == NULL);
+
+  freeList(list);
+  freeListBrief(bf);
+  
+  printf ("OK!\n");
+  return 0;
+}
